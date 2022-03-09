29584b28ada06f16a7096fe7dbb217a383e6b95a
darioplacidogandini <darioplacidogandini@outlook.com>
2022-03-09 11:19:26 -0300

    .

---
+++ src/main/java/com/dariogandini/portafolioapp/PortafolioappApplication.java
@@ -0,0 +1,13 @@
+package com.dariogandini.portafolioapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dariogandini/portafolioapp/PortafolioappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dariogandini.portafolioapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
