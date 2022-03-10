a23e348df892c483cba423c448fa4b1509b6eafc
darioplacidogandini <darioplacidogandini@outlook.com>
2022-03-10 05:01:22 -0300

    .

--- src/main/java/com/dariogandini/portafolioapp/controller/portafolioappController.java
+++ src/main/java/com/dariogandini/portafolioapp/controller/portafolioappController.java
@@ -1,13 +1,14 @@
 package com.dariogandini.portafolioapp.controller;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class portafolioappController {
 
     @GetMapping ("/portfolio")
-    public String mostrarPortfolio() {
-        return("index");    
+    public void mostrarPortfolio() {
+          
     }
 }
