9be2e7949efcfb9c839de04ceafd5f9e680883aa
darioplacidogandini <darioplacidogandini@outlook.com>
2022-04-10 20:12:18 -0300

    .

--- src/main/java/com/dariogandini/portafolioapp/model/JwtRequest.java
+++ src/main/java/com/dariogandini/portafolioapp/model/JwtRequest.java
@@ -3,12 +3,14 @@ package com.dariogandini.portafolioapp.model;
 import java.io.Serializable;
 
 public class JwtRequest implements Serializable {
+    
     private static final long serialVersionUID = 5926468583005150707L;
 
     private String username;
     private String password;
 
-    public JwtRequest() {
+    public JwtRequest()
+    {
 
     }
 
