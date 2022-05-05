b990b3f4918dafeb75ee385fdad61f43c8712d8a
darioplacidogandini <darioplacidogandini@outlook.com>
2022-05-05 11:36:55 -0300

    .

--- src/main/java/com/dariogandini/portafolioapp/PortafolioappApplication.java
+++ src/main/java/com/dariogandini/portafolioapp/PortafolioappApplication.java
@@ -1,32 +1,11 @@
 package com.dariogandini.portafolioapp;
 
-import com.dariogandini.portafolioapp.security.JWTAuthorizationFilter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class PortafolioappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PortafolioappApplication.class, args);
-	}
-
-	@EnableWebSecurity
-		@Configuration
-	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable().
-			addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class).
-			authorizeRequests().antMatchers( "/","/iniciar-sesion").permitAll().
-			anyRequest().authenticated();
-		}
 	}
 }
