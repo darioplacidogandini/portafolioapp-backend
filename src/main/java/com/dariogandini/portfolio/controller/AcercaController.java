63ce7ddaeef5c5f61b8fb1e3720565fa54c616e3
darioplacidogandini <darioplacidogandini@outlook.com>
2022-06-06 19:35:42 -0300

    .

--- src/main/java/com/dariogandini/portfolio/controller/AcercaController.java
+++ src/main/java/com/dariogandini/portfolio/controller/AcercaController.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"https://dariogandini-portfoliofrontend.firebaseapp.com","https://dariogandini-portfoliofrontend.web.app"})
+@CrossOrigin(origins = {"https://dariogandini-portfoliofrontend.firebaseapp.com","https://dariogandini-portfoliofrontend.web.app"},
+                methods = {RequestMethod.PUT})
 @RestController
 @RequestMapping("/api/acerca")
 public class AcercaController {
