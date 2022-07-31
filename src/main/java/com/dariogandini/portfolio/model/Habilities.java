392273066e549902bc17d70595a9544ddc3d36d6
darioplacidogandini <darioplacidogandini@outlook.com>
2022-07-30 23:31:22 -0300

    .

--- src/main/java/com/dariogandini/portfolio/model/Habilities.java
+++ src/main/java/com/dariogandini/portfolio/model/Habilities.java
@@ -1,10 +1,14 @@
 package com.dariogandini.portfolio.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "habilities")
 public class Habilities {
 
     @Id
--- src/main/java/com/dariogandini/portfolio/model/Projects.java
+++ src/main/java/com/dariogandini/portfolio/model/Projects.java
@@ -1,10 +1,14 @@
 package com.dariogandini.portfolio.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "projects")
 public class Projects {
 
     @Id
